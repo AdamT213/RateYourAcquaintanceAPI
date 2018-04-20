f8dfe65b43c39d206b6914131961c2fb29517125
AdamT213 <adt6261@gmail.com>
2018-04-20 17:09:27 -0400

    scoped reviews to people in routes to allow adding a review directly to a person

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :people
+  resources :people do 
+    resources :reviews 
+  end
   devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
