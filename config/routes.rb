e30132fbdca1c7b08bc4eeb8d889609269264d8a
AdamT213 <adt6261@gmail.com>
2018-04-26 15:28:01 -0400

    changing devise settings back to default to try and get user authentication with jwt to work

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   post 'auth_user' => 'authentication#authenticate_user'
 
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+  # devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
