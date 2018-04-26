086f3c44af0472f7768bc376a1ea956fd29c4d69
AdamT213 <adt6261@gmail.com>
2018-04-26 17:38:54 -0400

    experimenting with validating just user ans not password

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,8 @@
 class AuthenticationController < ApplicationController
   def authenticate_user
     user = User.find_by(email: params[:email])
-    if user && user.valid_password?(params[:password])
+    if user 
+      # && user.valid_password?(params[:password])
       render json: payload(user) 
     else
       render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
