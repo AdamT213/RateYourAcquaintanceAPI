8632e5f779aff6991d29e494085eb25bba07af75
AdamT213 <adt6261@gmail.com>
2018-05-14 17:49:50 -0400

    added belongs to user to review json to try to get user_id to show up

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :star_rating, :description, :user_id, :person_id 
-  belongs_to :person
+  belongs_to :person 
+  belongs_to :user
 end
